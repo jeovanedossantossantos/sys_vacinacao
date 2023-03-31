8802babd95437a2cc7f15400f388f45b198ddeea
Jeovane dos Santos Santos <60934938+jeovanedossantossantos@users.noreply.github.com>
2023-03-31 10:27:24 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int opcao;
+  do{
+    opcao = menu();
+    
+    switch(expression) {
+      case 1:
+        // code block
+        break;
+      case 2:
+        // code block
+        break;
+      case 3:
+        // code block
+        break;
+      case 4:
+        // code block
+        break;
+      case 5:
+        // code block
+        break;
+      case 6:
+        // code block
+        break;
+      default:
+        // code block
+    }
+    
+  }while(opcao !=0);
+
+    return 0;
+}
